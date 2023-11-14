603bfc533a86ff8045c5dbdc573a119c2754710a
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2023-11-14 15:43:21 +0600

    dropdown fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,35 @@ nestedItem.addEventListener("click", () => {
     nestedToggleMenu.classList.toggle("active")
     arrow.classList.toggle("active")
 })
+
+
+
+
+// custom dropdown functionality
+function setupCustomSelect(selectId) {
+    const selectContainer = document.getElementById(selectId);
+    const selectStyled = selectContainer.querySelector('.select-styled');
+    const selectOptions = selectContainer.querySelector('.select-options');
+
+    selectStyled.addEventListener('click', function () {
+        selectOptions.style.display = (selectOptions.style.display === 'block') ? 'none' : 'block';
+    });
+
+    selectContainer.querySelectorAll('.select-options div').forEach(function (option) {
+        option.addEventListener('click', function () {
+            selectStyled.innerText = this.innerText;
+            selectOptions.style.display = 'none';
+        });
+    });
+
+    // Close the dropdown when clicking outside of it
+    window.addEventListener('click', function (e) {
+        if (!e.target.closest('.custom-select') || e.target.closest('.custom-select') !== selectContainer) {
+            selectOptions.style.display = 'none';
+        }
+    });
+}
+
+// Set up the custom selects
+setupCustomSelect('customSelect1');
+setupCustomSelect('customSelect2');
